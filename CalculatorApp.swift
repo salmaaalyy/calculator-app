30dfa37ea74b427dd6555ab092824adfda8d6293
Salma <salmaaly47@gmail.com>
2021-09-03 21:37:51 -0700

    uploaded code files

---
+++ CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Salma Aly on 8/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
